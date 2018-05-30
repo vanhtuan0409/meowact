367c4e7970a1224d0f17057886ee816f900bfc86
Tuấn Vương <vanhtuan0409@gmail.com>
2018-05-30 14:58:44 +0700

    remove dead code

--- src/createElement.js
+++ src/createElement.js
@@ -1,7 +1,3 @@
-function isES6Class(v) {
-  return typeof v === "function" && /^\s*class\s+/.test(v.toString());
-}
-
 export default function createElement(tag, attributes, ...args) {
   const props = Object.assign({}, attributes);
   props.children = args.length > 0 ? [].concat(...args) : null;
